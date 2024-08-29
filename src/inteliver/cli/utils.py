90f908ccf3897b4c2e755d7c0513f478069baacb
Amir Lavasani <amirm.lavasani@gmail.com>
2024-08-29 09:38:04 +0330

    feat: add inteliver init command

--- src/inteliver/cli/utils.py
+++ src/inteliver/cli/utils.py
@@ -1,14 +1,14 @@
-import typer
+from pathlib import Path
+
+import yaml
 from art import text2art
 from colorama import Fore, Style
+from loguru import logger
 
-from inteliver.config import settings
 from inteliver.version import __template_version__, __version__
 
-cli = typer.Typer()
 
-
-def print_startup_logo():
+def print_inteliver_logo():
     version_colored = Fore.MAGENTA + Style.BRIGHT + "v" + __version__ + Style.RESET_ALL
     template_version_colored = (
         Fore.GREEN + Style.BRIGHT + "v" + __template_version__ + Style.RESET_ALL
@@ -23,23 +23,5 @@ def print_startup_logo():
     )
     print(logo)
     print(
-        f"\nRunning service {version_colored} 🚀 via fastapi-starter template {template_version_colored} 📦\n"
+        f"\nRunning service {version_colored} 🚀 via fastapi-starter {template_version_colored} 📦\n"
     )
-
-
-@cli.command()
-def run(host: str = settings.app_api_host, port: int = settings.app_api_port):
-    print_startup_logo()
-
-    from inteliver.main import run_service
-
-    run_service(host, port)
-
-
-@cli.command()
-def version():
-    print(f"🚀 {__version__}")
-
-
-if __name__ == "__main__":
-    cli()
